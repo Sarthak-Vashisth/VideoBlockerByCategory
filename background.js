f5e49291b67bc7631fb7e19b98514a3ca03d5a15
Sarthak Vashisth <sarthakvashisth@outlook.com>
2024-04-14 18:11:04 +0530

    Youtube video blocker first commit

---
+++ background.js
@@ -0,0 +1,1 @@
+// This is where you would handle any background tasks for your extension.
---
+++ contentScript.js
@@ -0,0 +1,12 @@
+alert("Youtube video blocker is running!")
+
+var element = document.getElementById('contents');
+element.parentNode.removeChild(element);
+
+// Get all video elements on the page
+// var videos = document.getElementById('contents');
+
+// // Loop through all video elements and remove them
+// for (var i = 0; i < videos.length; i++) {
+//   videos[i].parentNode.removeChild(videos[i]);
+// }
